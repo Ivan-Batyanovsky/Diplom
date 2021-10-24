482fb36b2d707b10f20ca3304d71727e8cfc4c82
Ivan-Batyanovsky <45259364+Ivan-Batyanovsky@users.noreply.github.com>
2021-10-24 23:16:58 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,11 @@
 #include "mainwindow.h"
-#include "myClasses/Buffers.h"
-#include "myClasses/Cube.h"
-#include "myClasses/Vec4f.h"
-#include "ToRefactor.h"
 
+#include <QWidget>
 #include <QPaintEvent>
 #include <QPixmap>
 #include <iostream>
 #include <cmath>
+#include <QDir>
 
 #define PI 3.14159265358979323846f
 
@@ -16,8 +14,10 @@ MainWindow::MainWindow(QWidget *parent)
 {
     // Getting triangles(3 consecutive points from 0, 3, 6 ... vector.size() - 4 make triangle) of objects
     Render_ = RenderBase();
-    Cube testCube(-1.0f, -1.0f, -6.0f, 2.0f);
-    Render_.loadCube(testCube);
+
+    // Load .obj files here using loadOBJ
+    // Example:
+    //    Render_.loadOBJ("C:/myOBJ.obj");
 }
 
 MainWindow::~MainWindow()
@@ -29,59 +29,59 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
 {
     switch (event->key()) {
         case Qt::Key_O:
-            Render_.changeScaling(Vec4f(0.8f, 0.8f, 1.2f));
+            Render_.changeScaling(Vec4f(-0.1f, -0.1f, -0.1));
             update();
             break;
         case Qt::Key_P:
-            Render_.changeScaling(Vec4f(1.2f, 1.2f, 0.8f));
+            Render_.changeScaling(Vec4f(0.1f, 0.1f, 0.1f));
             update();
             break;
         case Qt::Key_T:
-            Render_.changeTranslation(Vec4f(0.0f, -1.0f, 0.0f));
+            Render_.changeTranslation(Vec4f(0.0f, -0.3f, 0.0f));
             update();
             break;
         case Qt::Key_G:
-            Render_.changeTranslation(Vec4f(0.0f, 1.0f, 0.0f));
+            Render_.changeTranslation(Vec4f(0.0f, 0.3f, 0.0f));
             update();
             break;
         case Qt::Key_F:
-            Render_.changeTranslation(Vec4f(1.0f, 0.0f, 0.0f));
+            Render_.changeTranslation(Vec4f(0.3f, 0.0f, 0.0f));
             update();
             break;
         case Qt::Key_H:
-            Render_.changeTranslation(Vec4f(-1.0f, 0.0f, 0.0f));
+            Render_.changeTranslation(Vec4f(-0.3f, 0.0f, 0.0f));
             update();
             break;
         case Qt::Key_R:
-            Render_.changeTranslation(Vec4f(0.0f, 0.0f, 1.0f));
+            Render_.changeTranslation(Vec4f(0.0f, 0.0f, 0.3f));
             update();
             break;
         case Qt::Key_Y:
-            Render_.changeTranslation( Vec4f(0.0f, 0.0f, -1.0f));
+            Render_.changeTranslation( Vec4f(0.0f, 0.0f, -0.3f));
             update();
             break;
         case Qt::Key_W:
-            Render_.changeRotation(Vec4f(10.0f * PI / 360.0f, 0.0f, 0.0f));
+            Render_.changeRotation(Vec4f(5.0f * PI / 360.0f, 0.0f, 0.0f));
             update();
             break;
         case Qt::Key_S:
-            Render_.changeRotation(Vec4f(-10.0f * PI / 360.0f, 0.0f, 0.0f));
+            Render_.changeRotation(Vec4f(-5.0f * PI / 360.0f, 0.0f, 0.0f));
             update();
             break;
         case Qt::Key_A:
-            Render_.changeRotation(Vec4f(0.0f, 10.0f  * PI / 360.0f, 0.0f));
+            Render_.changeRotation(Vec4f(0.0f, 5.0f  * PI / 360.0f, 0.0f));
             update();
             break;
         case Qt::Key_D:
-            Render_.changeRotation(Vec4f(0.0f, -10.f  * PI / 360.0f, 0.0f));
+            Render_.changeRotation(Vec4f(0.0f, -5.f  * PI / 360.0f, 0.0f));
             update();
             break;
         case Qt::Key_Q:
-            Render_.changeRotation(Vec4f(0.0f, 0.0f, 10.0f  * PI / 360.0f));
+            Render_.changeRotation(Vec4f(0.0f, 0.0f, 5.0f  * PI / 360.0f));
             update();
             break;
         case Qt::Key_E:
-            Render_.changeRotation(Vec4f(0.0f, 0.0f, -10.0f  * PI / 360.0f));
+            Render_.changeRotation(Vec4f(0.0f, 0.0f, -5.0f  * PI / 360.0f));
             update();
             break;
     }
@@ -89,19 +89,21 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
 
 void MainWindow::paintEvent(QPaintEvent *event)
 {
-    size_t w = static_cast<size_t>(geometry().width());
-    size_t h = static_cast<size_t>(geometry().height());
+    size_t windowWidth = static_cast<size_t>(geometry().width());
+    size_t windowHeight = static_cast<size_t>(geometry().height());
 
     QPainter painter(this);
 
     // Model * View * Projection matrices
-    Mat44f modelViewProjectMat;
-    Render_.initMVPmatrix(w, h);
+    Render_.initMVPmatrix(windowWidth, windowHeight);
 
-    Render_.resizeBuffers(w, h);
-    // not necessary but prefer dark background
-    colorBackground(painter, w, h);
+    // Resize buffers after evety rebdered frame
+    Render_.resizeBuffers(windowWidth, windowHeight);
 
-    Render_.renderScene(painter, w, h);
+    // Colouring colour buffer with the scene
+    Render_.renderScene(windowWidth, windowHeight);
+
+    // Showing rendered scene in window
+    Render_.qtDraw(painter);
 }
 
